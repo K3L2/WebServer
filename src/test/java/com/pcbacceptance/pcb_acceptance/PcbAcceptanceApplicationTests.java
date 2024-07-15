e9111522f43cfaaef35bf63a092465fe54e2be25
sung <salamander011017@gmail.com>
2024-07-15 15:54:11 +0900

    test

---
+++ src/test/java/com/pcbacceptance/pcb_acceptance/PcbAcceptanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcbacceptance.pcb_acceptance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcbAcceptanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
